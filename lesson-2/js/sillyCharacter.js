ee9def6dd481dd433bd17c2efa0d2384d6943ec2
shiv-coder <shivi359@gmail.com>
2024-09-10 11:20:00 -0400

    just added SillCharacter.js file

---
+++ lesson-2/js/sillyCharacter.js
@@ -0,0 +1,75 @@
+/* 
+Project: "Silly Character Customizer"
+Objective:
+You’ll create a small interactive webpage that generates a random, 
+funny character with attributes like name, age, favorite food, and special powers. 
+The user can also customize some aspects like updating the character's name or age.
+
+Project Breakdown:
+Declare, Initialize, and Update Variables:
+
+You’ll create variables to store different character attributes 
+(name, age, favorite food, etc.).
+Users will have the option to update some of these variables, like 
+changing the character’s name or age.
+
+Use different data types for each attribute:
+Name (string)
+Age (number)
+Is the character a superhero? (boolean)
+Special powers (array)
+Favorite food (string)
+Conduct Basic Math Operations:
+
+You can randomly generate the character's age using math operations.
+You could also include a small feature where the character's age increases 
+or decreases when a button is clicked.
+Create and Manipulate Strings:
+
+Use string manipulation to generate a funny description of the character 
+based on the data. You can concatenate strings or use template literals to 
+display the character's attributes in a creative and humorous way.
+
+*/
+
+// Declare and initialize variables
+
+let characterName = "Alex";
+let age = Math.floor(Math.random() * 100); // Random age between 0 and 100
+let favoriteFood = "Pizza";
+let isSuperhero = Math.random() > 0.5; // Random boolean
+let specialPowers = ["Invisibility", "Super Strength", "Flying"];
+
+// Function to generate a random character description
+function generateCharacter() {
+    
+
+    // const description = `
+    //     Meet ${characterName}, a ${age}-year-old ${isSuperhero ? "superhero" : "regular person"} 
+    //     who loves ${favoriteFood} and has the power of ${powers}!
+    // `;
+    document.querySelector("#characterDescription").innerText = description;
+}
+
+// Functions to update character's age
+function increaseAge() {
+    
+}
+
+function decreaseAge() {
+    
+}
+
+// Function to update the character's description after changing age
+function updateCharacterDescription() {
+    const description = `
+        Meet ${characterName}, a ${age}-year-old ${isSuperhero ? "superhero" : "regular person"} 
+        who loves ${favoriteFood} and has the power of ${specialPowers[0]}!
+    `;
+    document.querySelector("#characterDescription").innerText = description;
+}
+
+// Add event listeners for buttons using querySelector
+document.querySelector('#generateButton').addEventListener('click', generateCharacter);
+document.querySelector('#increaseAgeButton').addEventListener('click', increaseAge);
+document.querySelector('#decreaseAgeButton').addEventListener('click', decreaseAge);
