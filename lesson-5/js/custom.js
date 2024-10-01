3f374982083d343cf577efd653ed1047a65168e4
shiv-coder <shivi359@gmail.com>
2024-10-01 11:15:03 -0400

    Added custom.js

--- lesson-5/js/custom.js
+++ lesson-5/js/custom.js
@@ -1,34 +1,56 @@
 // STEP 1a: Create reference for button element 'btn'
-
+const btn = document.querySelector('button');
 // STEP 1b: Create reference for primary heading 'h1'
-
+const heading1 = document.querySelector('h1');
 
 // STEP 3: Build an onclick event handler for 'btn' that invokes the displayMessage() function
-
+btn.onclick = function(){
+    let msgColor = displayMessage('This is a default','default');
+    heading1.style.color = msgColor;
+}
 
 // STEP 8d: Set the h1 element to the above color returned by the displayMessage function
 
 // STEP 2: Create a function called displayMessage()
-
-    // STEP 3a: Create a P element referenced by var msgPanel
-    
+function displayMessage(msgText,msgType){
+    // STEP 3a: Create a P element referenced by const msgPanel
+    const msgPanel = document.createElement('p');
     // STEP 3b: Add a class attribute, "msgPanel" (refer to the CSS above)
-        
+        msgPanel.setAttribute('class','msgPanel');
+        console.log(msgPanel);
     // STEP 3c: Insert the message text into the new paragraph element
-        
+        msgPanel.textContent = msgText;
+        console.log(msgPanel);
     // STEP 3d: Append the element to the document body
-    
+    document.body.appendChild(msgPanel);
 
     // STEP 4a: Create a close button referenced by var closeBtn
-    
+    const clostBtn = document.createElement('button');
     // STEP 4b: Add the letter 'x' to the button
-    
-    // STEP 4c: Add this new button element as a child of the msgPanel paragraph element created earlier
+    clostBtn.textContent = 'x';
+    //<button>x</button>
+    console.log(clostBtn);
+    // STEP 4c: Add this new button element as a child of the msgPanel 
+    //paragraph element created earlier
+    msgPanel.appendChild(clostBtn);
     
 
-    // STEP 5: Build an onclick event handler for the close button that removes the msgPanel from the DOM entirely using the removeChild() method
-    
-
+    // STEP 5: Build an onclick event handler for the close button that 
+    //removes the msgPanel from the DOM entirely using the removeChild() method
+    clostBtn.onclick= function(){
+        msgPanel.remove();
+    }
+        if(msgType === 'error'){
+            msgPanel.classList.add('error');
+        }else if(msgType === 'warning'){
+            msgPanel.classList.add('warning');
+        }else {
+            msgPanel.classList.add('default');
+        };
+        
+        let msgPanelColor = window.getComputedStyle(msgPanel).getPropertyValue('background-color');
+        console.log(msgPanelColor);
+        return msgPanelColor;
     // STEP 6a: Revise the function at STEP 2 to accept a parameter, 'msgText'
     // STEP 6b: At STEP 3 above, adapt the call to displayMessage() to include a string that should appear in the msgPanel
     // STEP 6c: Adjust STEP 3c above to use the passed-in parameter 'msgText'
@@ -43,7 +65,7 @@
     // STEP 8b: Return the background-color to the script that invoked this function
     
     // STEP 8c: Modify the line that calls this function above (in STEP 3) such that we set a variable to be equal to the name of this function.
-
+};
 
     // Next, open up the provided code for Lab 5 and review the instructions for the challenge.
 
